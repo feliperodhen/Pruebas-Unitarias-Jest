b1940df4d0d0b87016f8ed660fd0a9e7be9a1b03
Felipe Rodriguez <frodriguez@ecomsur.com>
2022-07-27 23:43:03 -0400

    pruebas en 02 template

---
+++ tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,14 @@
+import {getSaludo} from '../../src/base-pruebas/02-template-string.js';
+
+
+describe('Purbeas en 02-template-string', () => {
+
+    test('getSaludo debo de retornar "Hola Felipe', () => {
+
+        const name = 'Felipe';
+        const message = getSaludo(name);
+
+        expect(message).toBe(`Hola ${name}!`);
+    });
+
+});
